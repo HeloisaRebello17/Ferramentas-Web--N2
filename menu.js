73a061f41c131483104fd92d199e37af9801c827
RodrigoKlappoth <167566398+RodrigoKlappoth@users.noreply.github.com>
2024-06-07 16:39:56 -0300

    Menu Principal

---
+++ menu.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+        const toggle = document.getElementById(toggleId),
+              nav = document.getElementById(navId),
+              bodypd = document.getElementById(bodyId),
+              headerpd = document.getElementById(headerId),
+              botao = document.getElementById('botao');
+  
+        // Validate that all variables exist
+        if(toggle && nav && bodypd && headerpd){
+            toggle.addEventListener('click', ()=>{
+                // show navbar
+                nav.classList.toggle('show');
+                // change icon
+                toggle.classList.toggle('bx-x');
+                // add padding to body
+                bodypd.classList.toggle('body-pd');
+                // add padding to header
+                headerpd.classList.toggle('body-pd');
+                // Adicionar ou remover a classe de botao expandido
+                botao.classList.toggle('botao-expandido');
+                toggle.classList.toggle('bi-chevron-right');
+                toggle.classList.toggle('bi-chevron-left');
+  
+                if (nav.classList.contains('show')) {
+                    // Se o menu for expandido, adicione o texto
+                    if (!botao.querySelector('span')) {
+                        const btnText = document.createElement('span');
+                        btnText.textContent = 'Novo';
+                        botao.appendChild(btnText);
+                    }
+                } else {
+                    // Se o menu for recolhido, remova o texto, se existir
+                    const btnText = botao.querySelector('span');
+                    if (btnText) {
+                        btnText.remove();
+                    }
+                }
+            });
+            
+        }
+    }
+  
+    showNavbar('header-toggle','nav-bar','body-pd','header');
+  
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link');
+  
+    function colorLink(){
+        if(linkColor){
+            linkColor.forEach(l=> l.classList.remove('active'));
+            this.classList.add('active');
+        }
+    }
+    linkColor.forEach(l=> l.addEventListener('click', colorLink));
+  });
