1129c4d431a8275e8eb55a9a06b34262b32486d4
HeloisaRebello17 <101890320+HeloisaRebello17@users.noreply.github.com>
2024-06-30 03:16:59 -0300

    ajustando configurações

--- js/translate.js
+++ js/translate.js
@@ -10,7 +10,9 @@ const translations = {
         language: 'Language',
         currency: 'Currency',
         appearance: 'Appearance',
-        saveChanges: 'Save Changes'
+        saveChanges: 'Save Changes', 
+        SelectATheme: 'Select a theme',
+        SelectACurrency: 'Select a currency'
     },
     pt: {
         userName: 'Fulano',
@@ -23,7 +25,9 @@ const translations = {
         language: 'Idioma',
         currency: 'Moeda',
         appearance: 'Aparência',
-        saveChanges: 'Salvar Alterações'
+        saveChanges: 'Salvar Alterações',
+        SelectATheme: 'Selecione um tema',
+        SelectACurrency: 'Selecione uma moeda'
     },
     es: {
         userName: 'Usuario',
@@ -36,7 +40,9 @@ const translations = {
         language: 'Idioma',
         currency: 'Moneda',
         appearance: 'Apariencia',
-        saveChanges: 'Guardar Cambios'
+        saveChanges: 'Guardar Cambios',
+        SelectATheme: 'Selecciona un tema',
+        SelectACurrency: 'Seleccione una moneda'
     }
 };
 
@@ -49,8 +55,15 @@ function translatePage(language) {
 }
 
 function selectItem(selectedId, dropdownId, value) {
+    // Atualiza o texto do item selecionado
     document.getElementById(selectedId).innerText = value;
-    document.getElementById(dropdownId).style.display = 'none';
+
+    // Fecha o dropdown após selecionar um item
+    var dropdown = document.getElementById(dropdownId);
+    
+    dropdown.classList.remove('show'); // Remove a classe 'show' para ocultar visualmente o dropdown
+
+    // Chama a função de tradução se necessário
     if (value === 'Inglês') {
         translatePage('en');
     } else if (value === 'Português') {
@@ -60,12 +73,24 @@ function selectItem(selectedId, dropdownId, value) {
     }
 }
 
+
 function toggleDropdown(dropdownId) {
     var dropdown = document.getElementById(dropdownId);
     dropdown.style.display = dropdown.style.display === 'none' || dropdown.style.display === '' ? 'block' : 'none';
 }
 
-function selectTheme(selectedId, dropdownId, theme) {
-    selectItem(selectedId, dropdownId, theme);
-    document.body.setAttribute('data-theme', theme === 'Claro' ? 'light' : 'dark');
+// Função para selecionar o tema e fechar o dropdown
+function selectTheme(selectedItemId, dropdownId, theme) {
+    if (theme === 'Claro') {
+        document.body.classList.remove('dark-mode');
+    } else if (theme === 'Escuro') {
+        document.body.classList.add('dark-mode');
+    }
+    document.getElementById(selectedItemId).innerText = theme;
+    document.getElementById(dropdownId).classList.remove('show'); // Fechar o dropdown
 }
+
+
+
+
+
