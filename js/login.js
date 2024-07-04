329dc6fdf043e1db73104359a8d7c849302e874e
HeloisaRebello17 <heloisarebellocabral.79@gmail.com>
2024-07-04 01:25:08 -0300

    Responsividade

--- js/login.js
+++ js/login.js
@@ -2,3 +2,15 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     event.preventDefault();
     alert('Login efetuado com sucesso!');
 });
+
+
+const resetButton = document.getElementById('login-button');
+
+// Adiciona um ouvinte de evento de clique ao botão
+resetButton.addEventListener('click', () => {
+    // Define a URL para a qual deseja redirecionar
+    const novaPagina = 'menu com aviso.html'; // Substitua com a URL desejada
+
+    // Navega para a nova página
+    window.location.href = novaPagina;
+});
--- js/redefinir_senha1.js
+++ js/redefinir_senha1.js
@@ -2,3 +2,15 @@ document.getElementById('reset-form').addEventListener('submit', function(event)
     event.preventDefault();
     alert('Link para redefinição de senha enviado!');
 });
+
+// Seleciona o botão pelo ID
+const resetButton = document.getElementById('resetButton');
+
+// Adiciona um ouvinte de evento de clique ao botão
+resetButton.addEventListener('click', () => {
+    // Define a URL para a qual deseja redirecionar
+    const novaPagina = 'redefinir_senha.html'; // Substitua com a URL desejada
+
+    // Navega para a nova página
+    window.location.href = novaPagina;
+});
